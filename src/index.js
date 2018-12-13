27fc0901e99789ce251682b50ec8f02f2a093cf3
BAEK Y. JEONG <byjeong@us.ibm.com>
2018-12-13 15:51:59 -0600

    react hooks todo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import useTodoState from './useTodoState';
+import Typography from '@material-ui/core/Typography';
 
+const App = () => {
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+  return (
+    <div className="App">
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>      
+      <TodoForm saveTodo={todoText => {
+        console.log('#### todoText = ', todoText)
+        const trimmedText = todoText.trim()
+        if (trimmedText) {
+          addTodo(trimmedText)
+        }
+      }}>
+      </TodoForm>
+      <TodoList todos={todos} deleteTodo={deleteTodo}></TodoList>
+    </div>
+  )
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
